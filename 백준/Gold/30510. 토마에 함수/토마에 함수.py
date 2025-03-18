00cc24d10e9d663a9400e49cff051ff14180bbc5
maenwi <163512175+maenwi@users.noreply.github.com>
2025-03-19 04:36:01 +0900

    [Gold I] Title: 토마에 함수, Time: 868 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/30510. 토마에 함수/토마에 함수.py
@@ -0,0 +1,22 @@
+P, Q = map(int, input().split())
+
+def euler_totient_function(n):
+    result = n
+    # 소인수 분해는 sqrt 까지만 확인하면 됨.
+    for p in range(2, int(n ** (1/2)) + 1):
+        if n % p == 0: # 소인수인 p를 찾았다면, 
+            while n % p == 0: # p가 더 안 남아 있을 때 까지 나눠줌
+                n //= p
+            result -= result // p
+    
+    # sqrt(n) 보다 큰 소인수가 남아있다면 확인이 안 됐을 거고,
+    # 이 소인수는 유일함
+    if n > 1:
+        result -= result // n
+    return result
+
+answer = 0
+for q in range(1, Q // P + 1):
+    answer += euler_totient_function(q)
+
+print(answer + 1) # x == 0, x == 1
